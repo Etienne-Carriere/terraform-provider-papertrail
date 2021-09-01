63ffea3a97ae79b5ff88e863ab31d66c0e974e28
Etienne Carriere <etienne.a.carriere@socgen.com>
2021-09-01 18:01:05 +0200

    Fix entry point

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"github.com/Etienne-Carriere/terraform-provider-papertrail/papertrail"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: papertrail.Provider,
+		ProviderFunc: func() terraform.ResourceProvider {
+			return papertrail.Provider()
+		},
 	})
 }
